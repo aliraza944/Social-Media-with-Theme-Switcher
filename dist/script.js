f5814872094e5153075dffbbe94ee09ab22bd568
aliraza944 <alisabri022@gmail.com>
2022-10-19 14:27:39 +0500

    added the darl light mode styling button

--- dist/script.js
+++ dist/script.js
@@ -1,2 +1,2 @@
-"use strict";console.log("hello world");
+"use strict";for(var radioButtons=document.querySelectorAll('.toggle__wrapper input[type="radio"]'),i=0;i<radioButtons.length;i++)radioButtons[i].addEventListener("click",(function(){document.getElementById("dark").checked?(document.body.classList.add("dark"),document.body.classList.remove("light")):(document.body.classList.add("light"),document.body.classList.remove("dark"))}));
 //# sourceMappingURL=script.js.map
--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,15 @@
-console.log("hello world");
+const radioButtons = document.querySelectorAll(
+  '.toggle__wrapper input[type="radio"]'
+);
+
+for (let i = 0; i < radioButtons.length; i++) {
+  radioButtons[i].addEventListener("click", function () {
+    if (document.getElementById("dark").checked) {
+      document.body.classList.add("dark");
+      document.body.classList.remove("light");
+    } else {
+      document.body.classList.add("light");
+      document.body.classList.remove("dark");
+    }
+  });
+}
